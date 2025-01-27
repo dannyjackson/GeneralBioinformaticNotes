63a473144395891eaf6917f019c0441d00349519
Danny Jackson <dnjacks4@asu.edu>
2025-01-27 09:03:47 -0700

    Create Useful_Bash_Commands.sh

---
+++ Useful_Bash_Commands.sh
@@ -0,0 +1,3 @@
+# Useful Bash Commands
+less file.txt       # lets you read an entire file in an interactive way without outputting it all to the 
+du -hs /path/to/directory/*        # shows sizes of all files in the current directory; use du -hs /path/to/directory/ to get total size of a directory
